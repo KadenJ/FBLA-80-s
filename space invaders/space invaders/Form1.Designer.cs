0467ccdec3df7627d007c8d48761bd1ca3b9ff9f
KadenJ <72470602+KadenJ@users.noreply.github.com>
2021-09-03 09:36:01 -0400

    Initial commit

---
+++ space invaders/space invaders/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace space_invaders
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.GameTime = new System.Windows.Forms.Timer(this.components);
+            this.WorL = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.Image = global::space_invaders.Properties.Resources.tank;
+            this.Player.Location = new System.Drawing.Point(275, 473);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(65, 85);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Mishmash BRK", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.SystemColors.Control;
+            this.txtScore.Location = new System.Drawing.Point(12, 533);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(125, 16);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Score: 0";
+            this.txtScore.Click += new System.EventHandler(this.txtScore_Click);
+            // 
+            // GameTime
+            // 
+            this.GameTime.Interval = 20;
+            this.GameTime.Tick += new System.EventHandler(this.mainGameTimer);
+            // 
+            // WorL
+            // 
+            this.WorL.AutoSize = true;
+            this.WorL.Font = new System.Drawing.Font("Mishmash BRK", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WorL.ForeColor = System.Drawing.SystemColors.Control;
+            this.WorL.Location = new System.Drawing.Point(280, 272);
+            this.WorL.Name = "WorL";
+            this.WorL.Size = new System.Drawing.Size(0, 16);
+            this.WorL.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(684, 561);
+            this.Controls.Add(this.WorL);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.Player);
+            this.Name = "Form1";
+            this.Text = "SI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Timer GameTime;
+        private System.Windows.Forms.Label WorL;
+    }
+}
+
---
+++ space invaders/space invaders/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace space_invaders
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight;
+        int playerSpeed = 12;
+        int enemySpeed = 5;
+        int score = 0;
+        int enemyCount;
+        int enemyBulletTimer = 300;
+        int level;
+
+        PictureBox[] InvaderArray;
+
+        bool shooting;
+        bool isGameOver;
+        bool victory;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameSetup();
+        }
+
+        private void txtScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //basically update function from unity
+        private void mainGameTimer(object sender, EventArgs e)
+        {
+            txtScore.Text = "Score: " + score;
+
+            if (goLeft)
+            {
+                Player.Left -= playerSpeed;
+            }
+            if (goRight)
+            {
+                Player.Left += playerSpeed;
+            }
+
+            enemyBulletTimer -= 10;
+
+            if(enemyBulletTimer < 1)
+            {
+                enemyBulletTimer = 300;
+                makeBullet("sadBullet");
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if(x is PictureBox && (string)x.Tag == "Invaders")
+                {
+                    x.Left += enemySpeed;
+
+                    if(x.Left > 730)
+                    {
+                        x.Top += 65;
+                        x.Left = -80;
+                    }
+
+                    if (x.Bounds.IntersectsWith(Player.Bounds))
+                    {
+                        gameOver("You've been invaded!!");
+                    }
+
+                    foreach(Control y  in this.Controls)
+                    {
+                        if(y is PictureBox && (string)y.Tag == "bullet")
+                        {
+                            if (y.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                this.Controls.Remove(x);
+                                this.Controls.Remove(y);
+                                score += 1;
+                                enemyCount += 1;
+                                shooting = false;
+                            }                            
+                        }
+                    }                    
+                }
+
+                if(x is PictureBox &&(string)x.Tag == "bullet")
+                {
+                    x.Top -= 20;
+                    if (x.Top < 15)
+                    {
+                        this.Controls.Remove(x);
+                        shooting = false;
+                    }
+                }
+
+                if(x is PictureBox && (string)x.Tag == "sadBullet")
+                {
+                    x.Top += 20;
+
+                    if(x.Top> 620)
+                    {
+                        this.Controls.Remove(x);
+                    }
+
+                    if (x.Bounds.IntersectsWith(Player.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        gameOver("you've been killed");
+                    }
+                }
+            }
+                        
+            if(enemyCount > 8)
+            {
+                //need increase speed according to level
+                enemySpeed = 5;
+
+                
+            }
+
+            if (enemyCount == InvaderArray.Length)
+            {
+                win("you saved the planet!!" +
+                    "continue?");
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                goLeft = true;
+            }
+            if(e.KeyCode == Keys.Right || e.KeyCode == Keys.S)
+            {
+                goRight = true;
+            }
+            if(e.KeyCode == Keys.Space && shooting == false)
+            {
+                shooting = true;
+                makeBullet("bullet");
+            }
+            if(e.KeyCode == Keys.Enter && isGameOver == true)
+            {
+                removeAll();
+                gameSetup();
+            }
+            if (e.KeyCode == Keys.Enter && victory == true)
+            {
+                removeAll();
+                nextLevel();
+            }
+        }
+
+        private void makeInvaders()
+        {
+            InvaderArray = new PictureBox[15];
+
+            int left = 0;
+
+            for(int i = 0; i < InvaderArray.Length; i++)
+            {
+                InvaderArray[i] = new PictureBox();
+                InvaderArray[i].Size = new Size(60, 50);
+                InvaderArray[i].Image = Properties.Resources.aliean;
+                InvaderArray[i].Top = 5;
+                InvaderArray[i].Tag = "Invaders";
+                InvaderArray[i].Left = left;
+                InvaderArray[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(InvaderArray[i]);
+                left = left - 80;
+            }
+        }
+
+        private void Keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.A)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.S)
+            {
+                goRight = false;
+            }
+        }
+
+        private void gameSetup()
+        {
+            txtScore.Text = "Score: 0";
+            score = 0;
+            isGameOver = false;
+
+            enemyBulletTimer = 300;
+            enemySpeed = 5;
+            shooting = false;
+
+            makeInvaders();
+            GameTime.Start();
+
+            level = 1;
+            enemyCount = 0;
+        }
+
+        private void nextLevel()
+        {
+            enemyBulletTimer = 300;
+            enemySpeed = enemySpeed + 1;
+            shooting = false;
+
+            makeInvaders();
+            GameTime.Start();
+
+            level = 1;
+            enemyCount = 0;
+        }
+
+        private void gameOver(string message)
+        {
+            isGameOver = true;
+            GameTime.Stop();
+            txtScore.Text = "Score: " + score + " " + message; 
+        }
+
+        private void win(string message)
+        {
+            victory = true;
+            GameTime.Stop();
+            WorL.Text = message;
+        }
+
+        private void removeAll()
+        {
+            foreach (PictureBox i in InvaderArray)
+            {
+                this.Controls.Remove(i);
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if(x is PictureBox)
+                {
+                    if((string)x.Tag == "bullet" ||(string) x.Tag == "sadBullet")
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void makeBullet(string bulletTag)
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Image = Properties.Resources.c_users_anhar_ali_appdata_local_microsoft_windows_2;
+            bullet.Size = new Size(5, 20);
+            bullet.Tag = bulletTag;
+            bullet.Left = Player.Left + Player.Width / 2;
+
+            if((string) bullet.Tag == "bullet")
+            {
+                bullet.Top = Player.Top - 20;
+            }            
+            else if((string)bullet.Tag == "sadBullet")
+            {
+                bullet.Top = -100;
+            }
+
+            this.Controls.Add(bullet);
+            bullet.BringToFront();
+
+            
+        }
+    }
+}
