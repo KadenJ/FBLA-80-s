7e8563ea68bb6540470d657e93341ee14a6a39aa
KadenJ <72470602+KadenJ@users.noreply.github.com>
2021-09-22 10:26:33 -0400

    working on scoreboard
    using streamwriter

--- space invaders/space invaders/ScoreBoard.cs
+++ space invaders/space invaders/ScoreBoard.cs
@@ -8,26 +8,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace space_invaders
 {
     public partial class ScoreBoard : Form
     {
-        
+        int score = Form1.score;   
         public ScoreBoard()
         {
             InitializeComponent();
+
             
+
+            int[] scores = new int[5] {10000, 9500, 9000, 5000, 3000};
+            Array.Reverse(scores);
+
+            if(score >= scores[4] + 1 && score <= scores[3])
+            {
+                scores[4] = score;
+            }else if(score >= scores[3] + 1 && score <= scores[2])
+            {
+                scores[3] = score;
+            }else if(score >= scores[2] + 1 && score <= scores[1])
+            {
+                scores[2] = score;
+            }else if (score >= scores[1] + 1 && score <= scores[0])
+            {
+                scores[1] = score;
+            }else if (score>= scores[0] + 1)
+            {
+                //highscore
+                scores[0] = score;
+            }
+
+            Score1.Text = scores[0].ToString();
+            Score2.Text = scores[1].ToString();
+            Score3.Text = scores[2].ToString();
+            Score4.Text = scores[3].ToString();
+            Score5.Text = scores[4].ToString();
+
+            //need to check if txt file exists
+
+            using (StreamWriter sw = new StreamWriter("SIScoreBoard.txt"))
+            {
+                //should save array of scores
+                sw.WriteLine(scores);
+            }
+
+            using (StreamReader sr = new StreamReader("SIScoreBoard.txt"))
+            {
+                // load scoreboard
+            }
         }
 
         private void ScoreBoard_Load(object sender, EventArgs e)
         {
-            var scores = new ArrayList();
-            scores.Add(10000);
-            scores.Add(9500);
-            scores.Add(9000);
-            scores.Add(5000);
-            scores.Add(3000);
+            
 
             
         }
